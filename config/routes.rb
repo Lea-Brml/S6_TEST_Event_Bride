f8b05cf88e8c41fedbf3d1310b4f59d0d89dd81c
Lea-Brml <l.bramoulle@hotmail.fr>
2019-11-07 21:26:10 +0100

    maj photo profil

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end 
 
 
   get 'static_pages/index', to: 'static_pages#index'
